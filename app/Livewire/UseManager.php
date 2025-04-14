162f35840ac614072a3719effd17d52e27155bd8
Gabi <gabriel.perl.1996@gmail.com>
2025-04-14 19:18:04 +0200

    Livewire ejecutado

---
+++ app/Livewire/UseManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class UseManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.use-manager');
+    }
+}
